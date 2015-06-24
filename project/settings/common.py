da18241ed58201dae0dc55b4903a18228e045401
Sergey Petrov <petrov@trilan.ru>
2015-06-24 08:32:24 +0300

    fix settings and requirement for production

--- project/settings/common.py
+++ project/settings/common.py
@@ -61,13 +61,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 LANGUAGE_CODE = 'ru'
 
 LANGUAGES = (
--- project/settings/production.py
+++ project/settings/production.py
@@ -6,3 +6,10 @@ STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 PREPEND_WWW = False
 
 ALLOWED_HOSTS = ['188.226.193.98']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+    }
+}
